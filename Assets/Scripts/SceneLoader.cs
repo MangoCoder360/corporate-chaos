b899f018ad9b9955f73f4c6522f729f40408cdb6
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2024-05-22 20:20:42 -0700

    add scene transition

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,9 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName) {
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void DropCameraToScene(string sceneName)
+    {
+        GameObject camera = Camera.main.gameObject;
+        Rigidbody rb = camera.AddComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0, -500, 0), ForceMode.VelocityChange);
+        StartCoroutine(LoadSceneAfterDelay(sceneName, 2f));
+    }
+
+    private IEnumerator LoadSceneAfterDelay(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(sceneName);
     }
 }
